69c8631a58b574f26b234214d212dbf799f817f1
Nupur0128 <nupurvyas0128@gmail.com>
2025-09-23 15:02:50 +0530

    Remove deployment code and configurations

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import './App.css'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Bakrey from './bakrey'
 import IconHandler from './IconHandler'
 import Deals from './deals'
